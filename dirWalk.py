b3fe06b0e3137ef50a0ab1739aa39480f938f85c
King Mak <kingmak@hotmail.com>
2015-05-29 09:43:18 -0500

    add count for files and folders

--- dirWalk.py
+++ dirWalk.py
@@ -1,7 +1,12 @@
 import os
 
-def walk(directory = os.getcwd(), spaceCount = 0):
+fileCount = 0
+folderCount = 0
+
+def walk(directory, spaceCount = 0):
+    global fileCount, folderCount
     dirList = os.listdir(directory)
+    dirList.sort()
 
     for path in dirList:
 
@@ -9,11 +14,15 @@ def walk(directory = os.getcwd(), spaceCount = 0):
         
         if os.path.isfile(joinDir):
             print spaceCount * ' ' + '- ' + path
+            fileCount += 1
             
         elif os.path.isdir(joinDir):
             print spaceCount * ' ' + '* ' + path
+            folderCount += 1
             walk(joinDir, spaceCount + 1)
 
-        else: # else not needed, in for debug
+        else:
             print 'SOMETHING IS BROKEN, path is = ' + path
-walk()
+
+walk(os.getcwd())
+print '%d Files & %d Folders' % (fileCount, folderCount)
