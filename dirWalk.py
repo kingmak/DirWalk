0ec2e6061aaa2cc0e32cc2f80acb6d1ebd09d06e
King Mak <kingmak@hotmail.com>
2015-05-29 15:17:09 -0500

    Update dirWalk.py

--- dirWalk.py
+++ dirWalk.py
@@ -5,6 +5,7 @@ import os, sys, platform
 
 fileCount = 0
 folderCount = 0
+weird = 0
 
 fileColor = ''
 folderColor = ''
@@ -18,7 +19,7 @@ if 'Linux' in _os or 'Darwin' in _os:
 
 # too lazy to do the logic in one function
 def walkOne(directory = os.getcwd()):
-    global fileCount, folderCount
+    global fileCount, folderCount, weird
     dirList = os.listdir(directory)
 
     for path in dirList:
@@ -29,15 +30,16 @@ def walkOne(directory = os.getcwd()):
             os.system('echo ' + fileColor + '- ' + path + '"')
             fileCount += 1
             
-        else:
-            os.path.isdir(joinDir)
+        elif os.path.isdir(joinDir):
             os.system('echo ' + folderColor + '* ' + path + '"')
             folderCount += 1
 
+        else:
+            weird += 1
         #else: print 'SOMETHING IS BROKEN, path is = ' + path
 
 def walk(directory = os.getcwd() , spaceCount = 0):
-    global fileCount, folderCount
+    global fileCount, folderCount, weird
     dirList = os.listdir(directory)
 
     for path in dirList:
@@ -48,27 +50,34 @@ def walk(directory = os.getcwd() , spaceCount = 0):
             os.system('echo ' + fileColor + (spaceCount * ' ') + '- ' + path + '"')
             fileCount += 1
             
-        else:
-            os.path.isdir(joinDir)
+        elif os.path.isdir(joinDir):
             os.system('echo ' + folderColor + (spaceCount * ' ') + '* ' + path + '"')
             folderCount += 1
-            walk(joinDir, spaceCount + 1)
+            walk(joinDir, spaceCount + 2)
+
+        else:
+            weird += 1
 
 args = sys.argv
+usage = 'Usage:\n\npython dirWalk.py\npython dirWalk.py -c\npython dirWalk.py ~/\npython dirWalk.py ~/ -c\n\n* Note:\n+ green color and asterik means folder\n+ blue color and hypen means file\n+ -c means get files and folders only in the given directory\n+ weird objects are neither folders nor files\n'
 
-if len(args) == 1:
-    walk(os.getcwd())
+try:
+    if len(args) == 1:
+        walk(os.getcwd())
 
-elif len(args) == 2:
-    if args[1] == "-c":
-        walkOne()
-    else:
-        walk(args[1])
+    elif len(args) == 2:
+        if args[1] == "-c":
+            walkOne()
+        else:
+            walk(args[1])
 
-elif len(args) == 3:
-    walkOne(args[1])
+    elif len(args) == 3:
+        walkOne(args[1])
 
-else:
-    sys.exit('error')
+    else:        
+        sys.exit('error: invalid number of parameters\n' + usage)
 
-os.system('echo ' + statColor + ('\n%d Files & %d Folders in total' % (fileCount, folderCount)) + '"')
+except Exception, e:
+    sys.exit('error: ' + str(e) + '\n' + usage)
+
+os.system('echo ' + statColor + ('\n%d Files, %d Folders & %d weird objects in total' % (fileCount, folderCount, weird)) + '"')
