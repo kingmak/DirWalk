daadc02f69ad604690ee85e0743af70cac0b0a71
King Mak <kingmak@hotmail.com>
2015-07-03 10:01:49 -0500

    this is a windows only version

---
+++ WinWalk.py
@@ -0,0 +1,71 @@
+# Windows Version
+
+import os, sys
+
+fileCount = 0
+folderCount = 0
+weird = 0
+
+# too lazy to do the logic in one function
+def walkOne(directory = os.getcwd()):
+    global fileCount, folderCount, weird
+    dirList = os.listdir(directory)
+
+    for path in dirList:
+
+        joinDir = os.path.join(directory, path)
+
+        if os.path.isfile(joinDir):
+            print '- ' + path
+            fileCount += 1
+            
+        elif os.path.isdir(joinDir):
+            print '* ' + path
+            folderCount += 1
+
+        else:
+            weird += 1
+
+def walk(directory = os.getcwd() , spaceCount = 0):
+    global fileCount, folderCount, weird
+    dirList = os.listdir(directory)
+
+    for path in dirList:
+
+        joinDir = os.path.join(directory, path)
+        
+        if os.path.isfile(joinDir):
+            print spaceCount * ' ' + '- ' + path
+            fileCount += 1
+            
+        elif os.path.isdir(joinDir):
+            print spaceCount * ' ' + '* ' + path
+            folderCount += 1
+            walk(joinDir, spaceCount + 2)
+
+        else:
+            weird += 1
+
+args = sys.argv
+usage = 'Usage:\n\npython dirWalk.py\npython dirWalk.py -c\npython dirWalk.py ~/\npython dirWalk.py ~/ -c\n\n* Note:\n+ green color and asterik means folder\n+ blue color and hypen means file\n+ -c means get files and folders only in the given directory\n+ weird objects are neither folders nor files\n'
+
+try:
+    if len(args) == 1:
+        walk()
+
+    elif len(args) == 2:
+        if args[1] == '-c':
+            walkOne()
+        else:
+            walk(args[1])
+
+    elif len(args) == 3:
+        walkOne(args[1])
+
+    else:        
+        sys.exit('error: invalid number of parameters\n' + usage)
+
+except Exception, e:
+    sys.exit('error: ' + str(e) + '\n' + usage)
+
+print '\n%d Files, %d Folders & %d other objects in total' % (fileCount, folderCount, weird)
